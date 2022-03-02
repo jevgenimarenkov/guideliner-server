e662f6d94562cd207e580375be094a41aa76f74a
jevgenimarenkov <jevgeni.marenkov@gmail.com>
2022-03-02 15:38:34 +0200

    new logging part 1

--- src/main/java/ee/ttu/usability/guideliner/MvcConfig.java
+++ src/main/java/ee/ttu/usability/guideliner/MvcConfig.java
@@ -5,13 +5,14 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import ee.ttu.usability.guideliner.util.Logger;
 
 @Configuration
 @EnableWebMvc
 public class MvcConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        System.out.println("aaa");
+        Logger.Info("Adding registry");
         registry
                 .addResourceHandler("/guidelines/**")
                 .addResourceLocations("classpath:/guideliner/");
---
+++ src/main/java/ee/ttu/usability/guideliner/util/Logger.java
@@ -0,0 +1,7 @@
+package ee.ttu.usability.guideliner.util;
+
+public class Logger {
+    public static void Info(String message) {
+        System.out.println(message);
+    }
+}
