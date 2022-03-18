a30dcdbbafb781b8230cb7b5b4bbe6b2d6fb3570
jevgenimarenkov <jevgeni.marenkov@gmail.com>
2022-03-18 02:29:07 +0200

    refactoring

--- src/main/java/ee/ttu/usability/guideliner/estimation/adaptor/PasswordAdaptor.java
+++ src/main/java/ee/ttu/usability/guideliner/estimation/adaptor/PasswordAdaptor.java
@@ -2,16 +2,24 @@ package ee.ttu.usability.guideliner.estimation.adaptor;
 
 import ee.ttu.usability.guideliner.domain.element.form.Password;
 import ee.ttu.usability.guideliner.estimation.result.EvaluationResult;
-
+import ee.ttu.usability.guideliner.domain.attribute.Label;
 public class PasswordAdaptor extends AbstractFormAdaptor {
 
     public EvaluationResult execute(Password element) {
-        if (element.getLabel() != null && element.getLabel().getIsValued()!= null) {
-            return evaluateThatInputHasLabel("password", "Password element.", "Password element should have label.");
-        } else  if (element.getLabel() != null && element.getLabel().getHasText() != null) {
+        // Assuming getLabel() returns a Label object that could be null
+        if (element.getLabel() == null) {
+            throw new RuntimeException("Password element does not have a label.");
+        }
+
+        Label label = element.getLabel(); // Use a local variable to avoid multiple calls to getLabel()
+
+        // Combine checks for isValued and hasText to simplify logic
+        if (Boolean.TRUE.equals(label.getIsValued())) {
+            return evaluateThatInputHasLabel("password", "Password element.", "Password element should have a label.");
+        } else if (Boolean.TRUE.equals(label.getHasText())) {
             return evaluateThatInputHasValuedLabel("password", "Password element.", "Password element label should have value.");
         }
-        throw new RuntimeException("Do not know what to evaluate ");
-    }
 
+        throw new RuntimeException("Password element label does not have a value or text.");
+    }
 }
