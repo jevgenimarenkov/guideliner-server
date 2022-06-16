5d32617e92cffb0ee4969af212c015aadc981d7e
jevgenimarenkov <jevgeni.marenkov@gmail.com>
2022-06-16 05:05:10 +0300

    intorduced enum

--- src/main/java/ee/ttu/usability/guideliner/domain/guideline/Category.java
+++ src/main/java/ee/ttu/usability/guideliner/domain/guideline/Category.java
@@ -1,8 +1,17 @@
 package ee.ttu.usability.guideliner.domain.guideline;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public enum Category {
     AllGuidelines,
     WCAGGuideline,
     UsabilityGuideline,
     MobileUsabilityGuideline;
+
+    public static List<Category> getAllCategories() {
+        return Arrays.stream(Category.values())
+                .collect(Collectors.toList());
+    }
 }
