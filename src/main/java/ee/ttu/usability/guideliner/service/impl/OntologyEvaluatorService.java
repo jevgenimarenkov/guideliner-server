195829aa1484da0b4ab716ad4ec69d27855b818e
jevgenimarenkov <jevgeni.marenkov@gmail.com>
2022-08-13 09:44:10 +0300

    prepare failed element refactoring

--- src/main/java/ee/ttu/usability/guideliner/service/impl/OntologyEvaluatorService.java
+++ src/main/java/ee/ttu/usability/guideliner/service/impl/OntologyEvaluatorService.java
@@ -402,6 +402,16 @@ public class OntologyEvaluatorService {
 		//	driver.close();
 	}
 
+	public static <T> T createInstance(Class<T> clazz) {
+		try {
+			// Use reflection to instantiate the class represented by clazz
+			return clazz.getDeclaredConstructor().newInstance();
+		} catch (InstantiationException | IllegalAccessException e) {
+			// Handle the potential exceptions (e.g., no accessible no-argument constructor, class represents an abstract class, etc.)
+			throw new RuntimeException("Could not instantiate the class", e);
+		}
+	}
+
 	public WebDriver initialiseDriver() {
 		// TODO add some logic
 //		FirefoxProfile ffprofile = new FirefoxProfile();
